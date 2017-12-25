172b8531a475c0c6b26dab661986d89944f497a5
nagai_yoshihiko_gn <nagai_yoshihiko_gn@cyberagent.co.jp>
2017-12-25 16:53:09 +0900

    chapter5 始めたばかり

---
+++ src/main/scala/fp/chapter5/Chapter5Main.scala
@@ -0,0 +1,32 @@
+package fp.chapter5
+
+object Chapter5Main extends App {
+  trait Stream[+A] {
+    def headOption: Option[A] = this match {
+      case Empty => None
+      case Cons(h, _) => Some(h())
+    }
+    def toList: List[A] = this match {
+      case Empty => List()
+      case Cons(h, t) => h() :: t().toList
+    }
+  }
+  case object Empty extends Stream[Nothing]
+  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+  object Stream {
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
+    def empty[A]: Stream[A] = Empty
+    def apply[A](as: A*): Stream[A] = {
+      if (as.isEmpty) empty
+      else cons(as.head, apply(as.tail: _*))
+    }
+  }
+
+  println(Stream(1, 2, 3).toList)
+  println(Stream(1, 2, 3, 4, 5, 6))
+}
